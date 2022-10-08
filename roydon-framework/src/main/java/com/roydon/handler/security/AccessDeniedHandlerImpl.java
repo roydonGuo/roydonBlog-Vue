6704e96c90e0667983196dcd13a2c18c3bb9d7fb
roydonGuo <3133010060@qq.com>
2022-10-08 17:10:27 +0800

    认证失败操作

---
+++ roydon-framework/src/main/java/com/roydon/handler/security/AccessDeniedHandlerImpl.java
@@ -0,0 +1,30 @@
+package com.roydon.handler.security;
+
+import com.alibaba.fastjson.JSON;
+import com.roydon.domain.ResponseResult;
+import com.roydon.enums.AppHttpCodeEnum;
+import com.roydon.utils.WebUtils;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 自定义失败处理
+ * 授权过程中出现异常
+ */
+@Component
+public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        accessDeniedException.printStackTrace();
+        ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.NO_OPERATOR_AUTH);
+        String json = JSON.toJSONString(result);
+        WebUtils.renderString(response,json);
+
+    }
+}
