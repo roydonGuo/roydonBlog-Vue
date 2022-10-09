61b468ce24873be7bcebae5e3dea04e4527f06f4
roydonGuo <3133010060@qq.com>
2022-10-09 15:36:30 +0800

    全局异常处理

--- roydon-framework/src/main/java/com/roydon/handler/exception/GlobalExceptionHandler.java
+++ roydon-framework/src/main/java/com/roydon/handler/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.roydon.handler.exception;
 
 import com.roydon.domain.ResponseResult;
+import com.roydon.enums.AppHttpCodeEnum;
 import com.roydon.exception.SystemException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,9 +18,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(SystemException.class)
-    public ResponseResult handle(SystemException se) {
-        log.info("您的异常请注意查收：{}",se);
+    public ResponseResult systemExceptionHandle(SystemException se) {
+        log.info("您的异常请注意查收：{}", se);
         return ResponseResult.errorResult(se.getCode(), se.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseResult exceptionHandle(Exception e) {
+        log.info("您的异常请注意查收：{}", e);
+        return ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR.getCode(), e.getMessage());
     }
 
 }
