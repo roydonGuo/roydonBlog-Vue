fe53ffc35f41f5e0badad794cbc7526c61a369a8
roydonGuo <3133010060@qq.com>
2022-10-09 20:56:27 +0800

    yml

---
+++ roydon-framework/src/main/java/com/roydon/exception/SystemException.java
@@ -0,0 +1,25 @@
+package com.roydon.exception;
+
+import com.roydon.enums.AppHttpCodeEnum;
+
+public class SystemException extends RuntimeException {
+
+    private int code;
+
+    private String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public SystemException(AppHttpCodeEnum httpCodeEnum) {
+        super(httpCodeEnum.getMsg());
+        this.code = httpCodeEnum.getCode();
+        this.msg = httpCodeEnum.getMsg();
+    }
+
+}
