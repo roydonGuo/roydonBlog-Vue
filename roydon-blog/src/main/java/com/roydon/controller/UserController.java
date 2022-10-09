aa947f1b9ba347d703819b119c9e927ada49af42
roydonGuo <3133010060@qq.com>
2022-10-09 20:56:09 +0800

    用户

--- roydon-blog/src/main/java/com/roydon/controller/UserController.java
+++ roydon-blog/src/main/java/com/roydon/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.roydon.controller;
 
+import com.roydon.annotation.SystemLog;
 import com.roydon.domain.ResponseResult;
+import com.roydon.domain.entity.User;
 import com.roydon.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -18,5 +18,16 @@ public class UserController {
     @GetMapping("/userInfo")
     public ResponseResult userInfo(){
         return userService.userInfo();
+
+    }
+    @PutMapping("/userInfo")
+    @SystemLog(businessName = "更新用户")
+    public ResponseResult updateUserInfo(@RequestBody User user){
+        return userService.updateUserInfo(user);
+    }
+
+    @PostMapping("/register")
+    public ResponseResult register(@RequestBody User user){
+        return userService.register(user);
     }
 }
--- roydon-framework/src/main/java/com/roydon/service/impl/UserServiceImpl.java
+++ roydon-framework/src/main/java/com/roydon/service/impl/UserServiceImpl.java
@@ -1,14 +1,21 @@
 package com.roydon.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.roydon.domain.ResponseResult;
 import com.roydon.domain.entity.User;
 import com.roydon.domain.vo.UserInfoVo;
+import com.roydon.enums.AppHttpCodeEnum;
+import com.roydon.exception.SystemException;
 import com.roydon.mapper.UserMapper;
 import com.roydon.service.UserService;
 import com.roydon.utils.BeanCopyUtils;
 import com.roydon.utils.SecurityUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * 用户表(User)表服务实现类
@@ -24,8 +31,60 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         //根据用户id查询用户信息
         User user = getById(SecurityUtils.getUserId());
         //封装成UserInfoVo
-        UserInfoVo vo = BeanCopyUtils.copyBean(user,UserInfoVo.class);
+        UserInfoVo vo = BeanCopyUtils.copyBean(user, UserInfoVo.class);
         return ResponseResult.okResult(vo);
+    }
+
+    @Override
+    public ResponseResult updateUserInfo(User user) {
+        return ResponseResult.okResult(updateById(user));
+    }
+
+    @Resource
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public ResponseResult register(User user) {
+//对数据进行非空判断
+        if (StringUtils.isEmpty(user.getUserName())) {
+            throw new SystemException(AppHttpCodeEnum.USERNAME_NOT_NULL);
+        }
+        if (StringUtils.isEmpty(user.getPassword())) {
+            throw new SystemException(AppHttpCodeEnum.PASSWORD_NOT_NULL);
+        }
+        if (StringUtils.isEmpty(user.getEmail())) {
+            throw new SystemException(AppHttpCodeEnum.EMAIL_NOT_NULL);
+        }
+        if (StringUtils.isEmpty(user.getNickName())) {
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_EXIST);
+        }
+        //对数据进行是否存在的判断
+        if (userNameExist(user.getUserName())) {
+            throw new SystemException(AppHttpCodeEnum.USERNAME_EXIST);
+        }
+        if (nickNameExist(user.getNickName())) {
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_EXIST);
+        }
+        //...
+        //对密码进行加密
+        String encodePassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodePassword);
+        save(user);
+
+        return ResponseResult.okResult();
+    }
+
+    private boolean nickNameExist(String nickName) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getNickName, nickName);
+        return count(queryWrapper) > 0;
+    }
+
+    private boolean userNameExist(String userName) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUserName, userName);
+        return count(queryWrapper) > 0;
+
     }
 }
 
